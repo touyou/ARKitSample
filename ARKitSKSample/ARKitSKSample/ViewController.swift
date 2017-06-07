f1f0c21a3b8bf17a17b102a629845fd958498880
touyou <fujiyou1121@gmail.com>
2017-06-08 00:17:17 +0900

    modify sprite sample

--- ARKitSKSample/ARKitSKSample/ViewController.swift
+++ ARKitSKSample/ARKitSKSample/ViewController.swift
@@ -40,12 +40,8 @@ class ViewController: UIViewController, ARSKViewDelegate {
         // Run the view's session
         sceneView.session.run(configuration)
         
-        var translation = matrix_identity_float4x4
-        translation.columns.3.z = -0.2
-        let transform = simd_mul((sceneView.session.currentFrame?.camera.transform)!, translation)
-        
-        let anchor = ARAnchor(transform: transform)
-        sceneView.session.add(anchor: anchor)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(gestureRecognize:)))
+        sceneView.addGestureRecognizer(tapGesture)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -58,6 +54,22 @@ class ViewController: UIViewController, ARSKViewDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
+    }
+    
+    @objc
+    func handleTap(gestureRecognize: UITapGestureRecognizer) {
+        // Create anchor using the camera's current position
+        if let currentFrame = sceneView.session.currentFrame {
+            
+            // Create a transform with a translation of 0.2 meters in front of the camera
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.2
+            let transform = simd_mul(currentFrame.camera.transform, translation)
+            
+            // Add a new anchor to the session
+            let anchor = ARAnchor(transform: transform)
+            sceneView.session.add(anchor: anchor)
+        }
     }
     
     // MARK: - ARSKViewDelegate
